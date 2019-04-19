b3aa4657525fea150c95402b820515ca2e5851d4
Unknown <skyesladeut@outlook.com>
2019-04-19 15:28:34 -0600

    Deleted Messed Up project

--- Unity/ImpawsiblePursuit/Assets/Scripts/ScriptableObjectsScripts/BoolData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(menuName = "Data/bool")]
-public class BoolData : ScriptableObject
-{
-
-	public bool value;
-}
--- Unity/ImpawsiblePursuit/Assets/Scripts/ScriptableObjectsScripts/IntData.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(menuName = "Data/int")]
-public class IntData : ScriptableObject
-{
-	public int value;
-}
